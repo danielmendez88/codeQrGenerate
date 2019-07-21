94b014f94297069a66acc6dbcea733ad538400cf
daniel mendez cruz <danielmendez.88@live.com>
2019-07-21 13:11:28 -0500

    modificacion plantilla blade creedencial

--- resources/views/pages/credencial.blade.php
+++ resources/views/pages/credencial.blade.php
@@ -16,23 +16,18 @@
       </div>
       <div class="form-group has-feedback">
         <h5><label>Categoría:</label> {{ $categoria }}</h5>
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
         <h5><label>Puesto:</label> {{ $puesto }}</h5>
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
         <h5><label>Número Enlace:</label> {{ $numeroEnlace }}</h5>
-        <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
         <h5><label>O. Administrativo:</label> {{ $orgAdmin }}</h5>
-        <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
         <h5><label>A. de Adscripción:</label> {{ $areaAdscipcion }}</h5>
-        <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
       </div>
       <div class="row">
         <div class="col-xs-4">
@@ -50,11 +45,9 @@
 
     <div class="social-auth-links text-center">
       <p>- Puede Seguirnos en -</p>
+      <a href="http://www.icatech.gob.mx/" class="btn btn-block btn-social btn-google btn-flat"> Página de Icatech</a>
       <a href="https://www.facebook.com/Icatech-Chiapas-475079282568212/" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i>
-        Facebook</a><br/>
-      <label>Dirección:</label> 14 Poniente Norte No. 239 Colonia Moctezuma <br/>
-      <label>Teléfono:</label> +52 (961) 61-2-16-21 
-      input
+        Facebook</a>
     </div>
   </div>
   <!-- /.form-box -->
